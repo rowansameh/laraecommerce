c7fc9c4f7ff0ffafee7950cf089a8c5c61a04fae
Your Name <rowanebedy@gmail.com>
2023-04-28 01:44:22 +0300

    first commit

---
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -0,0 +1,78 @@
+    <nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index.html">
+            <i class="mdi mdi-camera-timer menu-icon"></i>
+            <span class="menu-title">Dashboard</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="pages/sales/basic_elements.html">
+            <i class="mdi mdi-sale menu-icon"></i>
+            <span class="menu-title">Sales</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
+            <i class="mdi mdi-view-list menu-icon"></i>
+            <span class="menu-title">Category</span>
+            <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="ui-basic">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="/admin/category/create">Add Category</a></li>
+                <li class="nav-item"> <a class="nav-link" href="/admin/category">View Category</a></li>
+            </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#product" aria-expanded="false" aria-controls="product">
+            <i class="mdi mdi-plus-circle menu-icon"></i>
+            <span class="menu-title">Products</span>
+            <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="product">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Add Product</a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> View Products </a></li>
+
+            </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/admin/brands">
+            <i class="mdi mdi-view-headline menu-icon"></i>
+            <span class="menu-title">Brands</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" data-bs-toggle="collapse" href="#auth" aria-expanded="false" aria-controls="auth">
+            <i class="mdi mdi-account-multiple-plus menu-icon"></i>
+            <span class="menu-title">Users</span>
+            <i class="menu-arrow"></i>
+            </a>
+            <div class="collapse" id="auth">
+            <ul class="nav flex-column sub-menu">
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/login.html"> Login </a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/login-2.html"> Login 2 </a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/register.html"> Register </a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/register-2.html"> Register 2 </a></li>
+                <li class="nav-item"> <a class="nav-link" href="pages/samples/lock-screen.html"> Lockscreen </a></li>
+            </ul>
+            </div>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="homeSlider/homeSlider.html">
+            <i class="mdi mdi-view-carousel menu-icon"></i>
+            <span class="menu-title">Home Slider</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="siteSetting/siteSetting.html">
+            <i class="mdi mdi-settings menu-icon"></i>
+            <span class="menu-title"> Site Settings</span>
+            </a>
+        </li>
+
+        </ul>
+    </nav>
