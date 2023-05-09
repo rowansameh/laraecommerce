7c1c03d7a7d61a24fbd1c026fdc328c3d48a7ad2
sara77kh <saraalaaeldin77@gmail.com>
2023-05-09 02:54:35 +0200

    update

---
+++ app/Http/Controllers/Admin/OrderController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use App\Http\Controllers\Controller;
+
+class OrderController extends Controller
+{
+    public function index(Request $request)
+   {
+      //$todayDate = Carbon::now();
+      //$orders = Order::whereDate('created_at',$todayDate)->paginate(10);
+       
+
+       $todayDate = Carbon::now()->format('Y-m-d');
+       $orders = Order::when($request->date != null, function($q) use($request) {
+
+                 return $q->whereDate('created_at',$request->date);
+              }, function($q) use ($todayDate){
+
+                return  $q->whereDate('created_at',$todayDate);
+              })
+                ->when($request->starus != null, function($q) use($request) {
+
+                  return $q->where('status_message',$request->status);
+               })
+               ->paginate(10);
+
+       return view ('admin.orders.index', compact('orders'));
+
+   }
+
+   public function show(int $orderId)
+   {
+    $order = Order::where('id',$orderId)->first();
+    if($order){
+        return view('admin.orders.view', compact('order'));
+    
+    }else{
+
+    return redirect('admin/orders')->with('message', 'Order Id not found');
+   }
+}
+
+
+    public function updateOrderStatus(int $orderId, Request $request)
+   {
+      $order = Order::where('id',$orderId)->first();
+      if($order){
+
+        $order->update([
+           'status_message' => $request->order_status
+        ]);
+        return redirect('admin/orders/'.$orderId)->with('message', 'Order Status Updated');
+      }else{
+
+         return redirect('admin/orders/'.$orderId)->with('message', 'Order Id not found');
+     }
+  }
+}
--- resources/views/layouts/inc/admin/sidebar.blade.php
+++ resources/views/layouts/inc/admin/sidebar.blade.php
@@ -7,9 +7,9 @@
             </a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="pages/sales/basic_elements.html">
+            <a class="nav-link" href="{{ url('admin/orders') }}">
             <i class="mdi mdi-sale menu-icon"></i>
-            <span class="menu-title">Sales</span>
+            <span class="menu-title">Orders</span>
             </a>
         </li>
         <li class="nav-item">
