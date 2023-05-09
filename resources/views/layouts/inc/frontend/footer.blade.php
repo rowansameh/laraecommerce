2ba77f4a3986cebb4578d5b5a430624f82f020ae
esraa203 <130873667+esraa203@users.noreply.github.com>
2023-05-09 19:56:32 +0300

    modify

--- resources/views/layouts/inc/frontend/footer.blade.php
+++ resources/views/layouts/inc/frontend/footer.blade.php
@@ -3,11 +3,11 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-3">
-                        <h4 class="footer-heading">Funda E-Commerce</h4>
+                        <h4 class="footer-heading">Funda Blossom</h4>
                         <div class="footer-underline"></div>
                         <p>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+                        Perfumes  website offer customers a convenient way to browse, compare, and purchase fragrances online.
+                        Discover a world of fragrances, including top-rated brands and exclusive releases, since the 1500s, to find your perfect scent.
                         </p>
                     </div>
                     <div class="col-md-3">
@@ -16,16 +16,15 @@
                         <div class="mb-2"><a href="{{ url('/')}}" class="text-white">Home</a></div>
                         <div class="mb-2"><a href="{{ url('/about-us')}}" class="text-white">About Us</a></div>
                         <div class="mb-2"><a href="{{ url('/contact-us')}}" class="text-white">Contact Us</a></div>
-                        <div class="mb-2"><a href="{{ url('/blogs')}}" class="text-white">Blogs</a></div>
-                        <div class="mb-2"><a href="#" class="text-white">Sitemaps</a></div>
+                        <div class="mb-2"><a href="{{ url('/my-account')}}" class="text-white">My Account</a></div>
+                        
+                        
                     </div>
                     <div class="col-md-3">
                         <h4 class="footer-heading">Shop Now</h4>
                         <div class="footer-underline"></div>
                         <div class="mb-2"><a href="{{ url('/collections')}}" class="text-white">Collections</a></div>
-                        <div class="mb-2"><a href="{{ url('/')}}" class="text-white">Trending Products</a></div>
-                        <div class="mb-2"><a href="{{ url('/')}}" class="text-white">New Arrivals Products</a></div>
-                        <div class="mb-2"><a href="{{ url('/')}}" class="text-white">Featured Products</a></div>
+                    
                         <div class="mb-2"><a href="{{ url('/cart')}}" class="text-white">Cart</a></div>
                     </div>
                     <div class="col-md-3">
@@ -33,17 +32,17 @@
                         <div class="footer-underline"></div>
                         <div class="mb-2">
                             <p>
-                                <i class="fa fa-map-marker"></i> #444, some main road, some area, some street, bangalore, india - 560077
+                                <i class="fa fa-map-marker"></i> Carrefour Alex City Center
                             </p>
                         </div>
                         <div class="mb-2">
                             <a href="" class="text-white">
-                                <i class="fa fa-phone"></i> +91 888-XXX-XXXX
+                                <i class="fa fa-phone"></i> 01277537518
                             </a>
                         </div>
                         <div class="mb-2">
                             <a href="" class="text-white">
-                                <i class="fa fa-envelope"></i> fundaofwebit@gmail.com
+                                <i class="fa fa-envelope"></i> admin@gmail.com
                             </a>
                         </div>
                     </div>
@@ -54,7 +53,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8">
-                        <p class=""> &copy; 2022 - Funda of Web IT Ecommerce. All rights reserved.</p>
+                        <p class=""> &copy; 2023 - Funda of Web Blossom. All rights reserved.</p>
                     </div>
                     <div class="col-md-4">
                         <div class="social-media">
