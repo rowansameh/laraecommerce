59b60e19a1aa4cf0c0e3f4dab88ac8ea47313ab2
sara77kh <saraalaaeldin77@gmail.com>
2023-05-13 19:31:24 +0200

    update

--- app/Http/Controllers/Frontend/UserController.php
+++ app/Http/Controllers/Frontend/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Models\User;
-use App\Models\Order;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
